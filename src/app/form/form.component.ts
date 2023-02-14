f66b0fdb5c0cd1a3d0ef966bf1d25f29b455a63e
TzipiReiss <116269752+TzipiReiss@users.noreply.github.com>
2023-02-14 18:18:18 +0200

    Update form.component.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import User from '../models/User';
 import { UserService } from '../services/user.service';
-import { angularMath } from 'angular-ts-math/dist/angular-ts-math/angular-ts-math';
 import Child from '../models/Child';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Workbook } from 'exceljs/dist/exceljs.min.js';
@@ -13,26 +12,18 @@ import * as fs from 'file-saver';
   styleUrls: ['./form.component.scss']
 })
 
-
 export class FormComponent implements OnInit {
   constructor(public userService: UserService) { }
   c: Child[] = []
   today = new Date();
-  hmoControl = new FormControl(null, Validators.required);
-  selectFormControl = new FormControl('', Validators.required);
   hmo: { name: string, code: number }[] =
     [{ name: 'Clalit', code: 0 }, { name: 'Leumit', code: 1 },
     { name: 'Maccabi', code: 2 }, { name: 'Meuhedet', code: 3 }]
-  data = [];
   save() {
     this.userService.currentUser.hmo = (Number)(this.userService.currentUser.hmo);
     this.userService.currentUser.gender = (Number)(this.userService.currentUser.gender);
     this.downloadExcel()
-    this.userService.addUser(this.userService.currentUser).subscribe(succ => {
-      console.log(succ);
-      this.data.push(this.userService.currentUser);
-
-    });
+    this.userService.addUser(this.userService.currentUser)
     this.userService.currentUser = new User(0, null, null, null, null, null, null, this.c);
     this.userService.currentChild = new Child(0, null, null, null, 0);
     this.userService.currentUser.children = []
@@ -47,41 +38,7 @@ export class FormComponent implements OnInit {
     console.log(this.userService.currentUser.children)
   }
 
-  // idVerification(id: String) {
-  //   if (id.length == 8 || id.length == 9) {
-  //     idNumber: Number = parseNum(id);
-  //      idArray:Number[]=[];
-
-  //     while (idArray.Count < 9) {
-  //                   int digit = idNumber % 10;
-  //       idNumber /= 10;
-  //       idArray.Add(digit);
-  //     }
-  //     var sum = 0;
-  //     for (int index = idArray.Count - 1; index >= 0; index--)
-  //     {
-  //       if (index % 2 != 0) {
-  //         var digit = idArray[index];
-  //         digit *= 2;
-  //         if (digit >= 10) {
-  //                           int last = digit % 10;
-  //                           int first = digit / 10;
-  //           digit = last + first;
-  //         }
-  //         idArray[index] = digit;
-  //       }
-  //       sum += idArray[index];
-  //     }
-
-  //     if ((sum >= 10) && (sum % 10 == 0)) {
-  //       return true;
-  //     }
-  //   }
-
-  //   return false;
-  // }
   downloadExcel() {
-
     //create new excel work book
     let workbook1 = new Workbook();
     let workbook2 = new Workbook();
@@ -129,15 +86,6 @@ export class FormComponent implements OnInit {
       worksheet2.addRow(temp)
     }
 
-    // for (let x1 of json_data) {
-    //   let x2 = Object.keys(x1);
-    //   let temp = []
-    //   for (let y of x2) {
-    //     temp.push(x1[y])
-    //   }
-    //   worksheet1.addRow(temp)
-    // }
-
     //set downloadable file name
     let fname = "user details"
 
@@ -146,21 +94,6 @@ export class FormComponent implements OnInit {
       let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
       fs.saveAs(blob, fname + '-' + new Date().valueOf() + '.xlsx');
     });
-  }
-  cheakDateUser() {
-    if (this.userService.currentUser.dateOfBirth) {
-      if (this.userService.currentUser.dateOfBirth.getFullYear < this.today.getFullYear)
-        return true;
-    }
-    return false;
-  }
-  cheakDateChild() {
-    if (this.userService.currentChild.dateOfBirth) {
-      if (this.userService.currentChild.dateOfBirth.getFullYear < this.today.getFullYear &&
-        this.userService.currentChild.dateOfBirth.getFullYear < this.userService.currentUser.dateOfBirth.getFullYear)
-        return true;
-    }
-    return false;
   }
 
   ngOnInit(): void {
